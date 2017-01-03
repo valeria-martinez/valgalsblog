9bece9abc2c59279cc6c60fcd8d658b46f352cae
valeriamartinez94 <valeriamartinez94@gmail.com>
2017-01-02 23:12:30 -0800

    gin n juice

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103000356) do
+ActiveRecord::Schema.define(version: 20170103062402) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "username"
+    t.index ["email"], name: "index_authors_on_email", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
