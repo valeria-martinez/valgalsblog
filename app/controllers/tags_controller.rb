5d70f202db02a32b480f632af4be64a4aadc863d
valeriamartinez94 <valeriamartinez94@gmail.com>
2017-01-02 21:57:08 -0800

    Add destroy tag functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,20 @@
 class TagsController < ApplicationController
 
 
-
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def index
+    @tags = Tag.all
+  end
+
+
+  def destroy
+    @tag = Tag.find(params[:id].to_i)
+    @tag.destroy
+    redirect_to tags_path
+  end
+
 
 end
